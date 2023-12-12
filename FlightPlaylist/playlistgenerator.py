5ab154051af610669309225fa9fdeaf093a6b762
vedachari <67667264+vedachari@users.noreply.github.com>
2023-12-12 13:30:47 -0500

    Update playlistgenerator.py
    Took out flight duration parameter. Now generates a playlist of 200 songs.

--- FlightPlaylist/playlistgenerator.py
+++ FlightPlaylist/playlistgenerator.py
@@ -4,18 +4,15 @@ import requests
 import pandas as pd
 
 # Set your Spotify API credentials ()
-CLIENT_ID =  #client id
+CLIENT_ID = #client id
 CLIENT_SECRET =  #client secret
 REDIRECT_URI =  #set to flight buddy website
-USERNAME = #client username
 
 #inputs: user_country: the ISO code the of the country they are traveling to
-#       flight_duration: duration of the flight
 #       sp: spotipy instance
-#       username: the user's username
 #       token: the oauth token
 #outputs: spotify playlist directly into the user's spotify library
-def get_playlist_by_country_and_length(user_country, flight_duration,token): #generates playlist from top 200 songs
+def get_playlist_by_country_and_length(user_country,token): #generates playlist from top 200 songs
     sp = spotipyInit() #initialize spotipy
     user_profile = sp.me()  #get user profile
 
@@ -29,16 +26,9 @@ def get_playlist_by_country_and_length(user_country, flight_duration,token): #ge
     playlist = sp.user_playlist_create(user_profile['id'], playlist_name,public=False, description = playlist_description)
     playlist_id = playlist['id']
 
-    #how many songs should be in the playlist (over estimate)
-    num_songs = int(flight_duration/2)
-
-    song_counter=0
     # Add tracks to the playlist
     for entry in tracks:
         sp.playlist_add_items(playlist_id=playlist_id, items=[entry['uri']])
-        song_counter+=1
-        if(song_counter>num_songs):
-            break
 
     #playlist was made and loaded
     print(f"Playlist '{playlist_name}' created with ID: {playlist_id}")
@@ -63,6 +53,5 @@ def spotipyInit(): #EDIT ONCE WE CONNECT TO FRONTEND
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                 client_secret=CLIENT_SECRET,
                                                 redirect_uri=REDIRECT_URI,
-                                                username = USERNAME,
                                                 scope='playlist-modify-private'))
     return sp
